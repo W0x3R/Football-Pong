a8e682bd89a9511490a53667f91990bf2d07e352
Stanislau Vyrski <vyr4376@yandex.ru>
2024-08-16 22:37:38 +0300

    feat: the height array from the checkgoalscoordinates function has been renamed and moved to the heightsofgoalarea module.  (Fri, Aug 16, 2024, 10:37:31 PM)

--- src/js/checkGoalsCoordinates.js
+++ src/js/checkGoalsCoordinates.js
@@ -1,27 +1,10 @@
+import { heightsOfGoalArea } from "./heightsOfGoalArea";
 import { getCurrentCenterOfField } from "./sizes/getCurrentCenterOfField";
 
 export const checkGoalsCoordinates = () => {
 	const center = getCurrentCenterOfField();
-	const heights = [
-		{ min: 570, height: 188 },
-		{ min: 540, height: 172 },
-		{ min: 520, height: 164 },
-		{ min: 500, height: 157 },
-		{ min: 470, height: 145 },
-		{ min: 446, height: 137 },
-		{ min: 420, height: 127 },
-		{ min: 388, height: 116 },
-		{ min: 360, height: 108 },
-		{ min: 332, height: 100 },
-		{ min: 300, height: 88 },
-		{ min: 282, height: 78 },
-		{ min: 261.5, height: 76 },
-		{ min: 246.5, height: 73 },
-		{ min: 223.5, height: 68 },
-		{ min: 208.5, height: 62 },
-		{ min: 0, height: 18 },
-	]
-	for (const item of heights) {
+	for (const item of heightsOfGoalArea
+	) {
 		if (center >= item.min) {
 			return item.height
 		}
