6773111862ed72ba6cbf8aed385a11844142d1ac
Stanislau Vyrski <vyr4376@yandex.ru>
2024-09-11 18:43:57 +0300

    feat: added fieldportraitimage variable to addstyleonchangeorientation module (Wed, Sep 11, 2024, 6:43:55 PM)

--- src/js/orientation/addStyleOnChangeOrientation.js
+++ src/js/orientation/addStyleOnChangeOrientation.js
@@ -1,8 +1,9 @@
 const portraitOrientationTitle = document.querySelector('.field-orientation__message')
-export const fieldImg = document.querySelector(".field__img");
+export const fieldImg = document.querySelector('.field__img')
+const fieldPortraitImage = document.querySelector('.field__portrait-image')
 
-export const addStyleOnChangeOrientation = (value, fieldImgSrc) => {
-	fieldImg.src = fieldImgSrc
-	fieldImg.classList[value]('field__portrait')
+export const addStyleOnChangeOrientation = (value) => {
+	fieldImg.classList[value]('field__img_hide')
+	fieldPortraitImage.classList[value]('field__portrait-image_show')
 	portraitOrientationTitle.classList[value]('field-orientation__message_show')
 }
