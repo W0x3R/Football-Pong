dc9f8025126feb2af27fe1d9c4e15ed4604c0038
Stanislau Vyrski <vyr4376@yandex.ru>
2024-09-11 18:45:22 +0300

    feat: removed unused imports and changed the timing of the checkorientation function call through the debounce decorator (Wed, Sep 11, 2024, 6:45:19 PM)

--- src/js/orientation/checkOrientation.js
+++ src/js/orientation/checkOrientation.js
@@ -1,10 +1,10 @@
-import { addStyleOnChangeOrientation } from "./addStyleOnChangeOrientation";
-import portraitImgSrc from '../../images/website-images/portrait-orientation-img.jpg'
-import fieldImgSrc from '../../images/website-images/field.jpg'
-import { debounce } from "../debounce";
+import { addStyleOnChangeOrientation } from './addStyleOnChangeOrientation'
+import { debounce } from '../debounce'
 
 export const checkOrientation = () => {
-	window.matchMedia("(orientation: portrait)").matches ? addStyleOnChangeOrientation('add', portraitImgSrc) : addStyleOnChangeOrientation('remove', fieldImgSrc)
-};
+	window.matchMedia('(orientation: portrait)').matches
+		? addStyleOnChangeOrientation('add')
+		: addStyleOnChangeOrientation('remove')
+}
 
-export const debounceCheckOrientation = debounce(checkOrientation, 200)
+export const debounceCheckOrientation = debounce(checkOrientation, 150)
