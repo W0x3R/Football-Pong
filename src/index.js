2a388ff940688da86af5e9c820d34e934a706018
Stanislau Vyrski <vyr4376@yandex.ru>
2024-09-11 18:46:03 +0300

    refactor: updated indentation of index.js file (Wed, Sep 11, 2024, 6:45:54 PM)

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
-import './style.scss';
-import { checkSessionScorePresence } from './js/playerScore/checkSessionScoreExists';
-import { debounceMovingBallRelativeFlag, movingBallRelativeFlag } from './js/ball/movingBallRelativeFlag';
-import { checkOrientation, debounceCheckOrientation } from './js/orientation/checkOrientation';
-import { clickEvents } from './js/eventHandlers/clickEvents';
-import { callEvents } from './js/eventHandlers/callEvents';
+import './style.scss'
+import { checkSessionScorePresence } from './js/playerScore/checkSessionScoreExists'
+import {
+	debounceMovingBallRelativeFlag,
+	movingBallRelativeFlag
+} from './js/ball/movingBallRelativeFlag'
+import { checkOrientation, debounceCheckOrientation } from './js/orientation/checkOrientation'
+import { clickEvents } from './js/eventHandlers/clickEvents'
+import { callEvents } from './js/eventHandlers/callEvents'
 
 // resize event
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
 	debounceMovingBallRelativeFlag()
 	debounceCheckOrientation()
-});
+})
 
 // window load event
-window.addEventListener("load", () => {
-	checkOrientation();
+window.addEventListener('load', () => {
+	checkOrientation()
 	movingBallRelativeFlag()
 	checkSessionScorePresence()
-});
+})
 
 window.addEventListener('click', (e) => {
 	callEvents(e, clickEvents)
