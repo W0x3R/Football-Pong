b6aaae94cdf03f739626294934416a261974c242
Stanislau Vyrski <vyr4376@yandex.ru>
2024-09-18 21:42:02 +0300

    refactor: changed the method of obtaining the screen width and height (Wed, Sep 18, 2024, 9:41:57 PM)

--- src/js/ball/generateRandomTopValue.js
+++ src/js/ball/generateRandomTopValue.js
@@ -1,10 +1,11 @@
-import { fieldImg } from "../orientation/addStyleOnChangeOrientation";
-import { getItemSizes } from "../sizes/getItemSizes";
-import { getBallBackOutField } from "./getBallBackOutField";
-import { ballImg } from "./setBallPosition";
+import { getItemSizes } from "../sizes/getItemSizes"
+import { getBallBackOutField } from "./getBallBackOutField"
+import { ballImg } from "./setBallPosition"
 
 export const generateRandomTopValue = () => {
-	let min = +getBallBackOutField();
-	let max = Math.floor(getItemSizes(fieldImg, 'height') - getItemSizes(ballImg, 'width') - min);
-	return Math.floor(Math.random() * (max - min + 1) + min);
+	let min = +getBallBackOutField()
+	let max = Math.floor(
+		getItemSizes(window, "innerHeight") - getItemSizes(ballImg, "width") - min
+	)
+	return Math.floor(Math.random() * (max - min + 1) + min)
 }
--- src/js/ball/setBallPosition.js
+++ src/js/ball/setBallPosition.js
@@ -1,16 +1,25 @@
-import { fieldImg } from "../orientation/addStyleOnChangeOrientation";
-import { getItemSizes } from "../sizes/getItemSizes";
-import { generateRandomTopValue } from "./generateRandomTopValue";
-import { getBallBackOutField } from "./getBallBackOutField";
+import { fieldImg } from "../orientation/addStyleOnChangeOrientation"
+import { getItemSizes } from "../sizes/getItemSizes"
+import { generateRandomTopValue } from "./generateRandomTopValue"
+import { getBallBackOutField } from "./getBallBackOutField"
 
-export const ballImg = document.querySelector(".field__ball");
+export const ballImg = document.querySelector(".field__ball")
 
 export const setBallHorizontalPosition = (neededPos) => {
-	let currentPos = neededPos === 'left' ? `${getBallBackOutField()}px` : getItemSizes(fieldImg, 'width') - getItemSizes(ballImg, 'width') - getBallBackOutField() + "px"
-	ballImg.style.left = currentPos;
+	let currentPos =
+		neededPos === "left"
+			? `${getBallBackOutField()}px`
+			: getItemSizes(window, "innerWidth") -
+			  getItemSizes(ballImg, "width") -
+			  getBallBackOutField() +
+			  "px"
+	ballImg.style.left = currentPos
 }
 
 export const setBallVerticalPosition = (neededPos) => {
-	let currentPos = neededPos === 'top' ? `${getBallBackOutField()}px` : generateRandomTopValue() + "px"
-	ballImg.style.top = currentPos;
+	let currentPos =
+		neededPos === "top"
+			? `${getBallBackOutField()}px`
+			: generateRandomTopValue() + "px"
+	ballImg.style.top = currentPos
 }
